a87afaa9342b83ecd217da727b2d0a52b2b06742
Andreas Fried <andi@0xaf.de>
2023-09-03 18:59:34 +0200

    dioxus: Implement rendering articles.

--- src/dioxus.rs
+++ src/dioxus.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 use dioxus::prelude::*;
 
 use crate::article_parser::{Token, Section};
@@ -7,46 +9,114 @@ pub fn launch() {
     dioxus_desktop::launch(app);
 }
 
-struct App {
-    game: Game,
+#[derive(Props)]
+struct HeadingNProps<'a> {
+    level: usize,
+    children: Element<'a>,
+}
+
+fn HeadingN<'a>(cx: Scope<'a, HeadingNProps<'a>>) -> Element {
+    match cx.props.level {
+	0 => panic!("Invalid heading level 0"),
+	1 => cx.render(rsx!( h1 { &cx.props.children })),
+	2 => cx.render(rsx!( h2 { &cx.props.children })),
+	3 => cx.render(rsx!( h3 { &cx.props.children })),
+	4 => cx.render(rsx!( h4 { &cx.props.children })),
+	5 => cx.render(rsx!( h5 { &cx.props.children })),
+	6 => cx.render(rsx!( h6 { &cx.props.children })),
+	_ => cx.render(rsx!( h6 { &cx.props.children })),
+    }
 }
 
 #[inline_props]
-fn Title(cx:Scope, tokens: Vec<Token>) -> Element {
+fn Token(cx: Scope, token: Token) -> Element {
+    let game = use_shared_state::<Game>(cx).unwrap();
+
+    match game.read().get_token_treatment(token) {
+        TokenTreatment::Blank => {
+            let dashes: Vec<&str> = std::iter::repeat("_").take(token.char_count()).collect();
+            let dashes = dashes.concat();
+            cx.render(rsx!(dashes))
+        }
+
+	TokenTreatment::Show => {
+	    cx.render(rsx!(token.get_str()))
+	}
+
+	TokenTreatment::Highlight => {
+	    cx.render(rsx!(
+		span {
+		    background_color: "cyan",
+
+		    token.get_str(),
+		}
+	    ))
+	}
+    }
+}
+
+#[inline_props]
+fn Title(cx: Scope, tokens: Vec<Token>) -> Element {
     cx.render(rsx! {
 	h1 {
 	    for token in tokens {
-		token.get_str()
+		cx.render(rsx!(Token { token: token.clone() }))
 	    }
 	}
     })
 }
 
 #[inline_props]
-fn ArticleParagraph(cx: Scope, tokens: Vec<Token>) -> Element {
-    cx.render(rsx!(
-	p {
-	    for token in tokens {
-		token.get_str()
-	    }
-	}
-    ))
-}
-
-#[inline_props]
 fn ArticleSection(cx: Scope, section: Section) -> Element {
     match section {
-	Section::Paragraph(tokens) => {
-	    cx.render(rsx!(ArticleParagraph { tokens: tokens.clone() }))
+        Section::Heading(level, tokens) => {
+	    cx.render(rsx!(
+		HeadingN {
+		    level: *level,
+		    for token in tokens {
+			cx.render(rsx!(Token { token: token.clone() }))
+		    }
+		}
+	    ))
 	}
-	_ => {
-	    cx.render(rsx!(p { "???" }))
+	Section::Paragraph(tokens) => {
+	    cx.render(rsx!(
+		p {
+		    for token in tokens {
+			cx.render(rsx!(Token { token: token.clone() }))
+		    }
+		}
+	    ))
+	}
+	Section::UnorderedList(list_items) => {
+	    cx.render(rsx!(
+		ul {
+		    for item in list_items {
+			cx.render(rsx!(
+			    li {
+				ArticleSections { sections: item.clone() }
+			    }))
+		    }
+		}
+	    ))
+	}
+	Section::OrderedList(list_items) => {
+	    cx.render(rsx!(
+		ol {
+		    for item in list_items {
+			cx.render(rsx!(
+			    li {
+				ArticleSections { sections: item.clone() }
+			    }))
+		    }
+		}
+	    ))
 	}
     }
 }
 
 #[inline_props]
-fn Article(cx: Scope, sections: Vec<Section>) -> Element {
+fn ArticleSections(cx: Scope, sections: Vec<Section>) -> Element {
     cx.render(rsx! {
 	for section in sections {
 	    rsx!(
@@ -57,27 +127,36 @@ fn Article(cx: Scope, sections: Vec<Section>) -> Element {
 }
 
 fn app(cx: Scope) -> Element {
-    let game = use_ref(cx, || Game::new());
+    use_shared_state_provider(cx, || Game::new());
+    let game = use_shared_state::<Game>(cx).unwrap();
 
     cx.render(rsx! (
 	button {
 	    onclick: move |_| {
-		game.write().load_article("en", "Rust (programming language)").unwrap();
-		println!("Loaded");
+		game.write().load_random_article().unwrap();
+		game.write().guess("language");
+		game.write().guess("the");
+		game.write().guess("rust");
+		game.write().selected_guess = String::from("rust");
             },
 
-	    "Example game"
+	    "Random article"
 	},
 
 	if let Some(wiki_article) = &game.read().wiki_article {
 	    rsx!(
-		Title { tokens: wiki_article.title.clone() },
+		div {
+		    font_family: "monospace",
+		    letter_spacing: "0.1em",
 
-		Article { sections: wiki_article.content.clone() }
+		    Title { tokens: wiki_article.title.clone() },
+
+		    ArticleSections { sections: wiki_article.content.clone() }
+		}
 	    )
 	} else {
 	    rsx!(
-		div { "Nothing to see" },
+		div { },
 	    )
 	}
     ))
